2fdbff7115415d9bb41f6c5f7973b7d1a282320b
ASK ONLINE <ios@askonlinesolutions.com>
2017-06-30 13:58:29 +0530

    Initial Commit

---
+++ CarHire/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CarHire
+//
+//  Created by ASK ONLINE  on 30/06/17.
+//  Copyright © 2017 ASK ONLINE. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
